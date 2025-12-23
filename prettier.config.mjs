c4b86613bb2d9f9ffc31ab1e8ebf45d2159a67f2
Howard Cui <howardcui3@gmail.com>
2025-12-23 19:43:00 +1100

    Configure eslint, prettier, husky

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+const config = {
+  plugin: ["prettier-plugin-tailwindcss"],
+  tailwindFunctions: ["mergeTwClasses"],
+  tabWidth: 2,
+  printWidth: 80,
+  trailingComma: "all",
+};
+
+export default config;
